270121c156835fae85058dc157661cc867358649
computerguycj <computerguycj@gmail.com>
2021-03-13 14:59:18 -0700

    Add project files.

---
+++ Controllers/ListController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using To_Do.Data;
+using To_Do.Models;
+
+namespace To_Do.Controllers
+{
+    public class ListController : Controller
+    {
+        private readonly ListItemContext _context;
+
+        public ListController(ListItemContext context)
+        {
+            _context = context;
+        }
+
+        // GET: List
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ListItems.ToListAsync());
+        }
+
+        // GET: List/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var listItem = await _context.ListItems
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (listItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(listItem);
+        }
+
+        // GET: List/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: List/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Text,Completed")] ListItem listItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(listItem);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(listItem);
+        }
+
+        // GET: List/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var listItem = await _context.ListItems.FindAsync(id);
+            if (listItem == null)
+            {
+                return NotFound();
+            }
+            return View(listItem);
+        }
+
+        // POST: List/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Text,Completed")] ListItem listItem)
+        {
+            if (id != listItem.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(listItem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ListItemExists(listItem.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(listItem);
+        }
+
+        // GET: List/Complete/5
+        public async Task<IActionResult> Complete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var listItem = await _context.ListItems
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (listItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(listItem);
+        }
+
+        // POST: List/Complete/5
+        [HttpPost, ActionName("Complete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CompleteConfirmed(int id)
+        {
+            var listItem = await _context.ListItems.FindAsync(id);
+            listItem.Completed = !listItem.Completed;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ListItemExists(int id)
+        {
+            return _context.ListItems.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Data/ListItemContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using To_Do.Models;
+
+namespace To_Do.Data
+{
+    public class ListItemContext : DbContext
+    {
+        public ListItemContext(DbContextOptions<ListItemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ListItem> ListItems { get; set; }
+    }
+}

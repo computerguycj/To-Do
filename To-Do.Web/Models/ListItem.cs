5f3110353399a3474ccacd04d11b49a64a4887db
computerguycj <computerguycj@gmail.com>
2021-03-14 12:48:39 -0600

    Require text field for list items

--- To-Do.Web/Models/ListItem.cs
+++ To-Do.Web/Models/ListItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace To_Do.Models
     public class ListItem
     {
         public int Id { get; set; }
+        [Required]
         public string Text { get; set; }
         public bool Completed { get; set; }
     }
